5af5555e6be3aacec27594fd617a41feb84bb10b
Tillson Galloway <tillson.galloway@gmail.com>
2015-05-07 20:36:27 -0400

    Finish Hue integration. TODO: Refactor

--- server.js
+++ server.js
@@ -8,7 +8,46 @@ bodyParser = require('body-parser'),
 ejs = require('ejs'),
 app = express(),
 Gmail = require("node-gmail-api"),
-hue = require("node-hue-api");
+Hue = require('philips-hue'),
+hue = new Hue();
+var path = require('path')
+
+
+function initalLoad() {
+  hue.getBridges(function(err, bridges){
+    if(err) return console.error(err);
+    console.log(bridges);
+
+    var bridge = bridges[0]; // use 1st bridge
+
+    console.log("You have 10 seconds to hit the bridge!  Run!!!");
+    setTimeout(function() {
+      hue.auth(bridge, function(err, username){
+        if(err) return console.error(err);
+        console.log("bridge: "+bridge);
+        console.log("username: "+username);
+      });
+    }, 10000);
+
+  });
+}
+
+var conf_file = process.env.HOME + '/.hue.json';
+hue.loadConfigFile(conf_file, function(err, conf){
+  if (err) {
+    initialLoad();
+    return;
+  }          
+  blinkLight({bri:254, xy:[.530,0.420]}, 1.0);
+
+});
+
+function blinkLight(state, seconds) {
+  hue.light(7).setState(state);
+  setTimeout(function() {
+    hue.light(7).setState({bri:254, hue: 14910, sat: 144});
+  }, seconds * 1000);
+}
 
 var emails = [];
 
@@ -23,15 +62,15 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(session({secret: nconf.get("session-secret"), resave: false, saveUninitialized: false}));
-app.use(passport.initialize());
-app.use(passport.session());
-
 function User(username, email) {
   this.username = username;
   this.email = email;
   this.hasNotified = false;
 }
+
+app.use(session({secret: nconf.get("session-secret"), resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new GoogleStrategy({
     clientID: nconf.get("gmail-client"),
@@ -83,20 +122,31 @@ setInterval(function() {
     s.on("data", function(d) {
 
       var shouldSendYo = true
+      var subjectHeader;
       for (var j = 0; j < d.payload.headers.length; j++) {
         var header = d.payload.headers[j];
 
-        if (header.name == "From" && header.value.indexOf("@apple.com") == -1) {
+        if (header.name == "From" && header.value.indexOf("@gmail.com") == -1) {
           shouldSendYo = false
         }
         if (header.name == "Subject" && header.value.indexOf("WWDC") == -1) {
           shouldSendYo = false
+        }
+        if (header.name == "Subject") {
+          subjectHeader = header;
         }
       }
       if (shouldSendYo) {
         yo.yo(yoName, function (err, res, body) {
           console.log("HEY, WE JUST YO'D " + yoName);
         })
+
+        // more parsing for hue lights
+        if (header.value.indexOf("won") > -1) {
+          blinkLight({bri:254, xy:[0.37,0.51721]}, 2.0);
+        } else {
+          blinkLight({bri:254, xy:[0.6679, 0.6679]}, 2.0);
+        }
         user.hasNotified = true;
       }
     })
@@ -104,5 +154,5 @@ setInterval(function() {
 }, 3000);
 
 app.listen(8080, function(){
-  console.log("Listening on port " +  8080);
+  console.log("Listening on port 8080");
 });
